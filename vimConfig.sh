bf0a5e2633dc0280c31124b42068928f754bd01c
Yeté Labarca <39843895+SaruMan64@users.noreply.github.com>
2022-01-07 11:35:43 -0300

    Update vimConfig.sh

--- vimConfig.sh
+++ vimConfig.sh
@@ -3,6 +3,6 @@
 cp .vimrc ~/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/sickill/vim-monokai.git
-cp -r vim-monokai/colors .vim/
+cp -r vim-monokai/colors ~/.vim/
 rm -rf vim-monokai
 vim  -c "PlugInstall"  -c q -c wq ~/.vimrc
